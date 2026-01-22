84bddf789fe01f95946edf314308227f261122db
fagnigue <devfullcoul@gmail.com>
2026-01-22 21:27:31 +0100

    chore: configurer pre-commit ruff

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     NEXTJS_BASE_URL: str
-    TIMEZONE: str
-
     DATABASE_URL: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
