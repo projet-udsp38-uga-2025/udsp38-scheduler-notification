32ccc2fbc3e28392502167b3195863800aaa1207
Gabriel Basile DELVA <gbsldelva@gmail.com>
2026-02-04 10:22:56 +0100

    fix: Utilisation de 2 topics différents pour l'envoi des notifs

--- app/core/config.py
+++ app/core/config.py
@@ -4,10 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     NEXTJS_BASE_URL: str
     DATABASE_URL: str
-
+    DEPLOYMENT_ENV: str
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 settings = Settings()
 TIMEZONE: str = "Europe/Paris"
-FIREBASE_MAIN_TOPIC = "news"
+FIREBASE_MAIN_TOPIC = "news" + settings.DEPLOYMENT_ENV
