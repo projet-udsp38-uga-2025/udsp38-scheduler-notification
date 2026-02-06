3faf98496a7ea00394eae4c752bcb40b66db56c6
Gabriel Basile DELVA <gbsldelva@gmail.com>
2026-02-06 12:18:22 +0100

    fix: Gestion de l'archivage des publications

--- app/api/router/jobs_router.py
+++ app/api/router/jobs_router.py
@@ -1,8 +1,15 @@
+from venv import logger
 from fastapi import APIRouter
 
+from app.dtos.requests.archiver_publication_dto import ProgrammerArchivageRequestDTO
 from app.dtos.requests.programmer_publication_dto import ProgrammerPublicationRequestDTO
+from app.dtos.requests.supprimer_job_dto import SupprimerJobRequestDTO
 from app.dtos.responses.job_programmation_dto import JobProgrammationDTO
-from app.services.jobs_service import programmer_publication
+from app.services.jobs_service import (
+    archiver_publication,
+    programmer_publication,
+    supprimer_job as service_supprimer_job,
+)
 
 jobs_router = APIRouter(prefix="/jobs", tags=["jobs"])
 
@@ -10,3 +17,11 @@ jobs_router = APIRouter(prefix="/jobs", tags=["jobs"])
 @jobs_router.post("/programmer-publication", response_model=JobProgrammationDTO)
 async def programmer(payload: ProgrammerPublicationRequestDTO):
     return programmer_publication(payload)
+
+@jobs_router.post("/programmer-archivage", response_model=JobProgrammationDTO)
+async def archiver(payload: ProgrammerArchivageRequestDTO):
+    return archiver_publication(payload)
+
+@jobs_router.delete("/supprimer-job")
+async def supprimer_job(payload: SupprimerJobRequestDTO):
+    return service_supprimer_job(payload)
--- app/dtos/responses/job_programmation_dto.py
+++ app/dtos/responses/job_programmation_dto.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
-
 class JobProgrammationDTO(BaseModel):
     job_id: str
     date_programmation: str
